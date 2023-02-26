51e8165ec05cce1aac4a88270f09c26d112bd584
Kavindu Sanjula Dasanayake <123143684+RavanaDevs@users.noreply.github.com>
2023-02-26 22:30:52 +0530

    Basic UI✅

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrackerComponent } from './components/tracker/tracker.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackerComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TaskListComponent,
+    TaskItemComponent,
+    TaskFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
